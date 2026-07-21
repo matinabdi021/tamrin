7601db461190f4d61a317d0c9bfccad05c3cbf61
Mtinabdi <matinabold@gmai.com>
2026-07-21 19:31:20 +0330

    rafactor: move to data-driven approch

--- MatinAbdi/One-Project/One-Project.js
+++ MatinAbdi/One-Project/One-Project.js
@@ -1,31 +1,49 @@
-const InputTodoO=document.getElementById('InputTodo');
-const BtnAddO=document.getElementById('BtnAdd');
-const InputTodo=document.getElementById('BtnDel');
-const UlTodoO=document.getElementById('ListItemUl');
-const showTextus=document.getElementById('TextCont')
+const InputTodoO = document.getElementById('InputTodo');
+const BtnAddO = document.getElementById('BtnAdd');
+const InputTodo = document.getElementById('BtnDel');
+const UlTodoO = document.getElementById('ListItemUl');
+const showTextus = document.getElementById('TextCont')
+//craet arrey
+const todoArray = JSON.parse(localStorage.getItem('Listdata')) || [];
+
+//function one
+
+function rendertodos() {
+    UlTodoO.innerHTML = '';
+    // creat ForEech
+    todoArray.forEach((item) => {
+        const card = document.createElement('li');
+        card.classList.add('Classli');
+        card.textContent = item;
+        UlTodoO.classList.add('Classul');
+        UlTodoO.appendChild(card);
+    });
+};
+
+// function addtodo
+
+function addtodo(text) {
+    todoArray.push(text);
+    localStorage.setItem('Listdata', JSON.stringify(todoArray));
+    rendertodos();
 
 
-//eventes
-BtnAddO.addEventListener('click',()=>{
-    const OutInput=InputTodoO.value.trim();
-    if(OutInput===""){
-        showTextus.textContent='لطفا مقداری را وارد کنید';
+}
+//Event Btn
+BtnAddO.addEventListener('click', () => {
+    const inputtext = InputTodoO.value.trim();
+    if (inputtext === "") {
+        showTextus.textContent = ' لطفا مقداری را وارد کنید';
         return;
-           
-        
-     
     }
-    showTextus.textContent='';
+    showTextus.textContent = '';
+    addtodo(inputtext);
+    InputTodoO.value = '';
+})
+rendertodos();
 
-    const card=document.createElement('li');
-    card.classList.add('Classli');
-    UlTodoO.classList.add('Classul')
-    card.textContent=OutInput;
 
-    UlTodoO.appendChild(card);
 
-    InputTodoO.value='';
-    
-  
 
-});
+
+
